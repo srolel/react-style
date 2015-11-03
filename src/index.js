c3372f66c6b37333e493732c3a6684d784d259cb
Sharon Rolel <sharonr@tipranks.com>
2015-11-03 11:41:04 +0200

    patch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import createStylesheet from './browser-styles.js';
 import reactribute from 'reactribute';
+import {extend} from './utils.js';
+
+export default styles => {
+	const stylesheet = createStylesheet(styles);
+	const decorator = reactribute(stylesheet.rules.map(r => ({
+		matcher: r.sel,
+		fn({props}) {
+			return {props: extend({}, props, {className: r.className})};
+		}
+	})));
+
+	return decorator;
+};
