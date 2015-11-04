eafe918b3210e13b4f19fa8a9b28d47b87f86a4c
Sharon Rolel <sharonr@tipranks.com>
2015-11-04 10:41:25 +0200

    patch

---
+++ src/create-stylesheet.js
@@ -0,0 +1,30 @@
+import detectNode from 'detect-node';
+import getStylesheet from './stylesheet-api.js';
+
+export const stylesheets = [];
+
+const createServerStylesheet = styles => {
+	const sheet = getStylesheet();
+	sheet.addRules(styles);
+	stylesheets.push(sheet);
+	return sheet;
+};
+
+const createBrowserStylesheet = styles => {
+	const styleElement = document.createElement('style');
+	document.head.appendChild(styleElement);
+	const sheet = styleElement.sheet;
+
+	const stylesheet = getStylesheet();
+
+	stylesheet.on('insertRule', r =>
+		sheet.insertRule(stylesheet.stringify(r)));
+
+	stylesheet.addRules(styles);
+
+	return stylesheet;
+};
+
+export default styles => detectNode
+	? createServerStylesheet(styles)
+	: createBrowserStylesheet(styles);
